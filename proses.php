44ae8e46b2d4af154c58fe4ae0ec5304efc6ea4a
mrahmatj <mrahmatj@student.telkomuniversity.ac.id>
2023-03-28 14:50:34 +0700

    v.1.2.1

--- proses.php
+++ proses.php
@@ -2,20 +2,25 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $angka_pertama = $_POST['angka_pertama'];
   $angka_kedua = $_POST['angka_kedua'];
-  $operator = $_POST['aritmatika'];
+  $operator = $_POST['operator'];
 
-  if ($operator === '+') {
-    $hasil = $angka_pertama + $angka_kedua;
-  } else if ($operator === '-') {
-    $hasil = $angka_pertama - $angka_kedua;
-  } else if ($operator === '*') {
-    $hasil = $angka_pertama * $angka_kedua;
-  } else if ($operator === '/') {
-    $hasil = $angka_pertama / $angka_kedua;
-  } else {
-    $output = "Operator tidak valid";
+  switch ($operator) {
+    case '+':
+      $hasil = $angka_pertama + $angka_kedua;
+      break;
+    case '-':
+      $hasil = $angka_pertama - $angka_kedua;
+      break;
+    case '*':
+      $hasil = $angka_pertama * $angka_kedua;
+      break;
+    case '/':
+      $hasil = $angka_pertama / $angka_kedua;
+      break;
+    default:
+      $output = "Operator tidak valid";
   }
 
-  echo "Hasil dari ".$angka_pertama." ".$operator." ".$angka_kedua." adalah ".$output;
+  echo "Hasil dari " . $angka_pertama . " " . $operator . " " . $angka_kedua . " adalah " . $output;
 }
 ?>
